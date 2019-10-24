2ab41a73561d902ca7d728c1e3e7bfd7845d822f
Joe Petrus <japetrus@gmail.com>
2019-10-24 10:37:25 -0400

    Update auto selection adjust example

--- workspace/auto_selection_adjust.py
+++ workspace/auto_selection_adjust.py
@@ -1,3 +1,5 @@
+# Note: depending on the number of channels and selections, this can take quite a while!
+
 from iolite.QtGui import QInputDialog
 from iolite.QtCore import QDateTime
 from sklearn.cluster import MeanShift, DBSCAN, OPTICS, SpectralClustering
@@ -18,6 +20,7 @@ def rle(inarray):
 		p = np.cumsum(np.append(0, z))[:-1] # positions
 		return(z, p, ia[i])
 
+input_group_name = None
 input_group_name = QInputDialog.getItem(None, "Auto Selection Adjuster", "Group:", data.selectionGroupNames(), 0, False)
 
 if not input_group_name:
@@ -27,8 +30,9 @@ group = data.selectionGroup(input_group_name)
 output_group = data.createSelectionGroup(input_group_name + '_auto_adj', group.type)
 
 # Specify which channels to use for the clustering:
-channel_names = ['Si29_ppm', 'Mg24_ppm', 'Al27_ppm', 'Fe57_ppm']
+#channel_names = ['Si29_ppm', 'Mg24_ppm', 'Al27_ppm', 'Fe57_ppm']
 #channel_names = data.timeSeriesNames(data.Output)
+channel_names = data.timeSeriesNames(data.Input)
 
 for selection in group.selections():	
 	# Create a data frame of the selection's data for channels specified above:
@@ -52,11 +56,11 @@ for selection in group.selections():
 	max_rl = np.amax(z)
 	start_index = p[np.argmax(z)]
 
-    t = data.timeSeries(channel_names[0]).timeForSelection(selection)
+	t = data.timeSeries(channel_names[0]).timeForSelection(selection)
 
-    start_ms = int(1000*(t[start_index]))
-    delta = np.median(np.diff(t))
-    end_ms =  start_ms + max_rl * delta * 1000
-    new_s = data.createSelection(output_group, QDateTime.fromMSecsSinceEpoch(start_ms), QDateTime.fromMSecsSinceEpoch(end_ms), selection.name)
+	start_ms = int(1000*(t[start_index]))
+	delta = np.median(np.diff(t))
+	end_ms =  start_ms + max_rl * delta * 1000
+	new_s = data.createSelection(output_group, QDateTime.fromMSecsSinceEpoch(start_ms), QDateTime.fromMSecsSinceEpoch(end_ms), selection.name)
 
 print('Done!')
