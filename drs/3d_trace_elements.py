961dfb4f8a753fd358446faf044e9147528941c0
Joe Petrus <japetrus@gmail.com>
2021-10-26 13:40:27 -0400

    Updated 3DTE to match built-in resources

--- drs/3d_trace_elements.py
+++ drs/3d_trace_elements.py
@@ -6,6 +6,28 @@
 #/ Version: 1.0
 #/ Contact: support@iolite-software.com
 
+"""
+Copyright (c) 2021 iolite software
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
 from iolite.QtGui import QLabel, QAbstractTableModel, QTableView, QTabWidget, QWidget, QSortFilterProxyModel, QLineEdit
 from iolite.QtGui import QToolButton, QMenu, QWidgetAction, QCheckBox,QAbstractItemView, QColor, QComboBox, QDoubleSpinBox, QColorDialog
 from iolite.QtGui import QWidget, QVBoxLayout, QSizePolicy, QDialog, QHBoxLayout, QShortcut, QKeySequence, QGroupBox, QPen
@@ -1560,7 +1582,7 @@ class CriteriaModel(QAbstractTableModel):
             'name': 'Unnamed',
             'criteria': '',
             'analytes': '',
-            'value': 100,
+            'value': 100.,
             'oxides': True,
             'oxide_forms': []
         })
@@ -1618,7 +1640,7 @@ class CriteriaModel(QAbstractTableModel):
             elif index.column() == 2:
                 return self.criteria[index.row()]['analytes']
             elif index.column() == 3:
-                return self.criteria[index.row()]['value']
+                return float(self.criteria[index.row()]['value'])
             elif index.column() == 4:
                 return self.criteria[index.row()]['oxides']
             elif index.column() == 5:
@@ -1640,7 +1662,7 @@ class CriteriaModel(QAbstractTableModel):
         elif index.column() == 2:
             self.criteria[index.row()]['analytes'] = value
         elif index.column() == 3:
-            self.criteria[index.row()]['value'] = value
+            self.criteria[index.row()]['value'] = float(value)
         elif index.column() == 5:
             self.criteria[index.row()]['oxide_forms'] = str(value).split(',')
 
